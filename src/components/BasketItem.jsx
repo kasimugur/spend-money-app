2f099adeed4807c81653dd68c9065fc1d4948ce2
kasimugur <kasiugur7@gmail.com>
2024-06-03 13:36:49 +0300

    basketItem css add

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,8 +1,9 @@
-
+ 
+ import "../css/basketItem.css"
 export default function BasketItem({item, product}) {
   return (
     <>
-<li className="basket-item">{product.title} x {item.amount}</li>
+<li className="basket-item">{product.title} <span>x {item.amount}</span></li>
     </>
   )
 }
